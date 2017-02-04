de0132a9ca65e24f7eeb17ea5921e3e0311d3d3f
Vitaliy Tsutsman <vitaliyacm@gmail.com>
2017-02-04 12:21:26 +0200

    Imports optimization.

--- src/main/java/com/thecoffeine/auth/AuthApplication.java
+++ src/main/java/com/thecoffeine/auth/AuthApplication.java
@@ -2,8 +2,6 @@ package com.thecoffeine.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
--- src/main/java/com/thecoffeine/auth/config/InternationalizationConfig.java
+++ src/main/java/com/thecoffeine/auth/config/InternationalizationConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import java.util.Locale;
