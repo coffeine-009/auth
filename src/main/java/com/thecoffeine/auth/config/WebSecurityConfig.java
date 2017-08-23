6e21265de1966f9d72327371ca4a47243d13b452
Vitaliy Tsutsman <vitaliyacm@gmail.com>
2017-08-23 22:59:54 +0300

    Fix get user info by token.

--- src/main/java/com/thecoffeine/auth/config/WebSecurityConfig.java
+++ src/main/java/com/thecoffeine/auth/config/WebSecurityConfig.java
@@ -1,12 +1,9 @@
 package com.thecoffeine.auth.config;
 
 import com.thecoffeine.auth.model.matcher.CsrfSecurityRequestMatcher;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.ManagementServerProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -18,9 +15,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @Order( ManagementServerProperties.ACCESS_OVERRIDE_ORDER)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private AuthenticationManager authenticationManager;
 
     /**
      * Override this method to configure the {@link HttpSecurity}. Typically subclasses
@@ -39,6 +33,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http//TODO: disable csrf for AJAX
             .cors()
             .and()
+            .requestMatchers()
+            .antMatchers( "/", "/login", "/oauth/authorize", "/oauth/confirm_access" )
+            .and()
             .authorizeRequests()
                 .antMatchers( "/security/**" ).permitAll()
                 .antMatchers( "/user/info" ).permitAll()
@@ -52,53 +49,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
                 .logout()
                 .permitAll();
-    }
-
-    /**
-     * Used by the default implementation of {@link #authenticationManager()} to attempt
-     * to obtain an {@link AuthenticationManager}. If overridden, the
-     * {@link AuthenticationManagerBuilder} should be used to specify the
-     * {@link AuthenticationManager}.
-     * <p>
-     * <p>
-     * The {@link #authenticationManagerBean()} method can be used to expose the resulting
-     * {@link AuthenticationManager} as a Bean. The {@link #userDetailsServiceBean()} can
-     * be used to expose the last populated {@link UserDetailsService} that is created
-     * with the {@link AuthenticationManagerBuilder} as a Bean. The
-     * {@link UserDetailsService} will also automatically be populated on
-     * {@link HttpSecurity#getSharedObject(Class)} for use with other
-     * {@link SecurityContextConfigurer} (i.e. RememberMeConfigurer )
-     * </p>
-     * <p>
-     * <p>
-     * For example, the following configuration could be used to register in memory
-     * authentication that exposes an in memory {@link UserDetailsService}:
-     * </p>
-     * <p>
-     * <pre>
-     * &#064;Override
-     * protected void configure(AuthenticationManagerBuilder auth) {
-     * 	auth
-     * 	// enable in memory based authentication with a user named
-     * 	// &quot;user&quot; and &quot;admin&quot;
-     * 	.inMemoryAuthentication().withUser(&quot;user&quot;).password(&quot;password&quot;).roles(&quot;USER&quot;).and()
-     * 			.withUser(&quot;admin&quot;).password(&quot;password&quot;).roles(&quot;USER&quot;, &quot;ADMIN&quot;);
-     * }
-     *
-     * // Expose the UserDetailsService as a Bean
-     * &#064;Bean
-     * &#064;Override
-     * public UserDetailsService userDetailsServiceBean() throws Exception {
-     * 	return super.userDetailsServiceBean();
-     * }
-     *
-     * </pre>
-     *
-     * @param auth the {@link AuthenticationManagerBuilder} to use
-     * @throws Exception
-     */
-    @Override
-    protected void configure( AuthenticationManagerBuilder auth ) throws Exception {
-        auth.parentAuthenticationManager( this.authenticationManager );
     }
 }
